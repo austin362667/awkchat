c650e368205d89304466899c54ff0fe7087277ca
austin362667 <austin362667@gmail.com>
2020-09-16 23:23:59 +0800

    get name

--- chat.ts
+++ chat.ts
@@ -3,6 +3,7 @@ import {
   isWebSocketCloseEvent,
 } from "https://deno.land/std@0.60.0/ws/mod.ts";
 import { v4 } from "https://deno.land/std@0.60.0/uuid/mod.ts";
+import { generate } from "https://deno.land/std@0.60.0/uuid/v1.ts";
 // import { camelize } from "./camelize.ts";
 
 const users = new Map<string, WebSocket>();
@@ -14,8 +15,18 @@ function broadcast(message: string, senderId?: string): void {
   }
 }
 
+function getRandomInt(max:number) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function generateName() {
+  const adj = ['藍色', '藍', '紅色', '紅', '黃色', '黃', '綠色', '綠', '紫色', '黑白', '黯綠', '褐', '金黃','美麗','漂亮','開開心心','怒','尷尬','時間管理','努力的','亭亭玉立','忿忿不平','嘻嘻哈哈','淺藍','閃亮','暗', '好奇', '帥氣']
+  const n = ['寶寶','獨角獸','大師','打工仔','老司機','美少女','葛格','恐龍化石','小可愛','小姐姐','大學生','蟑螂','小男孩', '大美女', '北鼻','教授','帥哥','饒舌歌手','湯姆貓','米奇老鼠','小飛象','小公主','美人魚','花木蘭'] 
+  return adj[getRandomInt(adj.length)] + n[getRandomInt(n.length)];
+}
+
 export async function chat(ws: WebSocket): Promise<void> {
-  const userId = v4.generate();
+  const userId = generateName();//v4.generate();
 
   // Register user connection
   users.set(userId, ws);
