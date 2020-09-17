e95e5c4b80eb533438209e8378ff05df6669a735
austin362667 <austin362667@gmail.com>
2020-09-18 00:55:51 +0800

    add video

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
+app.get('/video', (req, res) => {
+  res.sendFile(__dirname + '/public/video.html');
+});
+
 io.on('connection', (socket) => {
   console.log('a user connected');
 });
@@ -26,6 +30,40 @@ io.on('connection', (socket) => {
   });
 });
 
+
+function findNowRoom(client) {
+  return Object.keys(client.rooms).find(item => {
+    return item !== client.id
+  });
+}
+
+io.on('connection', client => {
+  console.log(`socket 用戶連接 ${client.id}`);
+
+  client.on('joinRoom', room => {
+    console.log(room);
+    
+    const nowRoom = findNowRoom(client);
+    if (nowRoom) {
+      client.leave(nowRoom);
+    }
+    client.join(room, () => {
+      io.sockets.in(room).emit('roomBroadcast', '已有新人加入聊天室！');
+    });
+  });
+
+  client.on('peerconnectSignaling', message => {
+    console.log('接收資料：', message);
+ 
+    const nowRoom = findNowRoom(client);
+    client.to(nowRoom).emit('peerconnectSignaling', message)
+  });
+
+  client.on('disconnect', () => {
+    console.log(`socket 用戶離開 ${client.id}`);
+  });
+});
+
 http.listen(443, () => {
-  console.log('listening on *:3000');
+  console.log('listening on *:443');
 });
