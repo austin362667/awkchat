966ace8e0fb4baad2cf54ad42eca776fae6f5046
austin362667 <austin362667@gmail.com>
2020-09-21 00:05:18 +0800

    add chatting booth

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
+var express = require('express');
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+// const redis = require("redis");
+// const path = require("path");
 
+// const subscriber = redis.createClient();
+// const publisher = redis.createClient();
+// subscriber.on("error", function (err) {
+//   console.log("Error " + err);
+// });
+// publisher.on("error", function (err) {
+//   console.log("Error " + err);
+// });
+var loopLimit = 0;
+// room = 0;
+// match = [];
 // var options = {
 //   key: fs.readFileSync('/etc/letsencrypt/live/lattemall.company/privkey.pem'),
 //   ca: [fs.readFileSync('/etc/letsencrypt/live/lattemall.company/fullchain.pem')],
@@ -9,65 +23,340 @@ var io = require('socket.io')(http);
 // };
 // https.createServer(options,app);
 
+
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname+'/public/index.html');
 });
 
-app.get('/video', (req, res) => {
-  res.sendFile(__dirname + '/public/video.html');
+app.get('/room', (req, res) => {
+  res.sendFile(__dirname+'/public/room.html');
 });
+
+// app.post('/match', async (req, res) => {
+//   // let { queueName, email } = req.body;
+//   // queueName = 'match-service';
+//   message="austin362667@gmail.com"
+//   subscriber.subscribe("waiting room");
+// 	res.status(200).send({
+// 		"message": "match request queued successfully."
+// 	})
+// })
+
+app.use(express.static('public'))
+
+
+// subscriber.on("subscribe", function(channel, count) {
+//   console.log("subscribe queued!");
+//   publisher.publish(channel, message);
+// });
+
+// subscriber.on("message", function(channel, message) {
+//   match.push(message);
+//   console.log(match.length);
+//   console.log("Subscriber received message in channel '" + channel + "': " + message);
+//   if (match.length === 2) {
+//     console.log("It's two of : ", match);
+
+//       console.log('join room:', room);
+//       io.on('connection', (socket) => {
+//         socket.join(room);
+//       });
+//       // io.sockets.in(room).emit('return room', room);
+
+//       room+=1;
+//     subscriber.unsubscribe();
+//     // subscriber.quit();
+//     // publisher.quit();
+//     match = [];
+//     console.log("Now queue is consumed : ", match);
+//   }else{
+//     console.log('create room:', room);
+//     io.on('connection', (socket) => {
+//       socket.join(room);
+//     });
+//     // io.sockets.in(room).emit('return room', room);
+
+//   }
+// });
+
+// app.get('/video', (req, res) => {
+//   res.sendFile(__dirname + '/public/video.html');
+// });
 
 io.on('connection', (socket) => {
   console.log('a user connected');
-});
-
-io.on('connection', (socket) => {
   msg = JSON.stringify({name:'[公告]', mssg:'歡迎新用戶加入！'})
   io.emit('chat message', msg);
   socket.on('disconnect', () => {
     console.log('user disconnected');
-  });
-});
-
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
+    msg = JSON.stringify({name:'[公告]', mssg:'一位用戶下線了..'})
     io.emit('chat message', msg);
   });
 });
 
+io.on('connection', (socket) => {
 
-function findNowRoom(client) {
-  return Object.keys(client.rooms).find(item => {
-    return item !== client.id
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
   });
+});
+//   socket.on('addRoom', room => {
+//     socket.join(room)
+//     //(1)發送給在同一個 room 中除了自己外的 Client
+//     // socket.to(room).emit('addRoom', `已有新人加入 ${room} 聊天室！`)
+//     //(2)發送給在 room 中所有的 Client
+//     io.sockets.in(room).emit('addRoom', `已加入 ${room} 聊天室！`)
+// })
+
+// socket.on('leaveRoom', room => {
+//   socket.leave(room);
+//   //(1)發送給在同一個 room 中除了自己外的 Client
+//   // socket.to(room).emit('addRoom', `已有新人加入 ${room} 聊天室！`)
+//   //(2)發送給在 room 中所有的 Client
+//   io.sockets.in(room).emit('room chat message', `已離開 ${room} 聊天室！`)
+// })
+
+//   socket.on('room chat message', (room, msg) => {
+//     console.log(`room: ${room},     msg: ${msg}`)
+//     io.sockets.in(room).emit('room chat message', msg);
+//   });
+
+// });
+
+// Entire gameCollection Object holds all games and info
+var gameId;
+var gameCollection =  new function() {
+
+  this.totalGameCount = 0,
+  this.gameList = []
+
+};
+
+function buildGame(socket) {
+
+
+ var gameObject = {};
+ gameObject.id = (Math.random()+1).toString(36).slice(2, 18);
+ gameObject.playerOne = socket.username;
+ gameObject.playerTwo = null;
+ gameCollection.totalGameCount ++;
+ gameCollection.gameList.push({gameObject});
+
+ console.log("Game Created by "+ socket.username + " w/ " + gameObject.id);
+ io.emit('gameCreated', {
+  username: socket.username,
+  gameId: gameObject.id
+});
+gameId = gameObject.id
+socket.join(gameId); // join room  
+
+
 }
 
-io.on('connection', client => {
-  console.log(`socket 用戶連接 ${client.id}`);
+function killGame(socket) {
 
-  client.on('joinRoom', room => {
-    console.log(room);
+  var notInGame = true;
+  for(var i = 0; i < gameCollection.totalGameCount; i++){
+
+    var gameId = gameCollection.gameList[i]['gameObject']['id']
+    var plyr1Tmp = gameCollection.gameList[i]['gameObject']['playerOne'];
+    var plyr2Tmp = gameCollection.gameList[i]['gameObject']['playerTwo'];
     
-    const nowRoom = findNowRoom(client);
-    if (nowRoom) {
-      client.leave(nowRoom);
+    if (plyr1Tmp == socket.username){
+      --gameCollection.totalGameCount; 
+      console.log("Destroy Game "+ gameId + "!");
+      gameCollection.gameList.splice(i, 1);
+      console.log(gameCollection.gameList);
+      socket.emit('leftGame', { gameId: gameId });
+      io.emit('gameDestroyed', {gameId: gameId, gameOwner: socket.username });
+      notInGame = false;
+    } 
+    else if (plyr2Tmp == socket.username) {
+      gameCollection.gameList[i]['gameObject']['playerTwo'] = null;
+      console.log(socket.username + " has left " + gameId);
+      socket.emit('leftGame', { gameId: gameId });
+      console.log(gameCollection.gameList[i]['gameObject']);
+      notInGame = false;
+
+    } 
+
+  }
+
+  if (notInGame == true){
+    socket.emit('notInGame');
+  }
+
+
+}
+
+function gameSeeker (socket) {
+  ++loopLimit;
+  if (( gameCollection.totalGameCount == 0) || (loopLimit >= 20)) {
+
+    buildGame(socket);
+    loopLimit = 0;
+
+  } else {
+    var rndPick = Math.floor(Math.random() * gameCollection.totalGameCount);
+    if (gameCollection.gameList[rndPick]['gameObject']['playerTwo'] == null)
+    {
+      gameCollection.gameList[rndPick]['gameObject']['playerTwo'] = socket.username;
+      socket.emit('joinSuccess', {
+        gameId: gameCollection.gameList[rndPick]['gameObject']['id'] });
+        gameId = gameCollection.gameList[rndPick]['gameObject']['id']
+      socket.join(gameId);// join room
+        console.log( socket.username + " has been added to: " + gameCollection.gameList[rndPick]['gameObject']['id']);
+
+    } else {
+
+      gameSeeker(socket);
     }
-    client.join(room, () => {
-      io.sockets.in(room).emit('roomBroadcast', '已有新人加入聊天室！');
+  }
+}
+
+
+// Chatroom
+
+var numUsers = 0;
+
+io.on('connection', function (socket) {
+  var addedUser = false;
+
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', function (data) {
+    console.log(data);
+    // we tell the client to execute 'new message'
+    socket.broadcast.in(gameId).emit('new message', {
+      username: socket.username,
+      message: data
     });
   });
 
-  client.on('peerconnectSignaling', message => {
-    console.log('接收資料：', message);
- 
-    const nowRoom = findNowRoom(client);
-    client.to(nowRoom).emit('peerconnectSignaling', message)
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', function (username) {
+    if (addedUser) return;
+
+    // we store the username in the socket session for this client
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
   });
 
-  client.on('disconnect', () => {
-    console.log(`socket 用戶離開 ${client.id}`);
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', function () {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
   });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', function () {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function () {
+    if (addedUser) {
+      --numUsers;
+      killGame(socket);
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
+
+
+  socket.on('joinGame', function (){
+    console.log(socket.username + " wants to join a game");
+
+    var alreadyInGame = false;
+
+    for(var i = 0; i < gameCollection.totalGameCount; i++){
+      var plyr1Tmp = gameCollection.gameList[i]['gameObject']['playerOne'];
+      var plyr2Tmp = gameCollection.gameList[i]['gameObject']['playerTwo'];
+      if (plyr1Tmp == socket.username || plyr2Tmp == socket.username){
+        alreadyInGame = true;
+        console.log(socket.username + " already has a Game!");
+
+        socket.emit('alreadyJoined', {
+          gameId: gameCollection.gameList[i]['gameObject']['id']
+        });
+
+      }
+
+    }
+    if (alreadyInGame == false){
+
+
+      gameSeeker(socket);
+      
+    }
+
+  });
+
+
+  socket.on('leaveGame', function() {
+
+
+    if (gameCollection.totalGameCount == 0){
+     socket.emit('notInGame');
+     
+   }
+
+   else {
+    killGame(socket);
+  }
+
 });
+
+});
+
+
+// function findNowRoom(client) {
+//   return Object.keys(client.rooms).find(item => {
+//     return item !== client.id
+//   });
+// }
+// 
+// io.on('connection', client => {
+//   console.log(`socket 用戶連接 ${client.id}`);
+
+//   client.on('joinRoom', room => {
+//     console.log(room);
+    
+//     const nowRoom = findNowRoom(client);
+//     if (nowRoom) {
+//       client.leave(nowRoom);
+//     }
+//     client.join(room, () => {
+//       io.sockets.in(room).emit('roomBroadcast', '已有新人加入聊天室！');
+//     });
+//   });
+
+//   client.on('peerconnectSignaling', message => {
+//     console.log('接收資料：', message);
+ 
+//     const nowRoom = findNowRoom(client);
+//     client.to(nowRoom).emit('peerconnectSignaling', message)
+//   });
+
+//   client.on('disconnect', () => {
+//     console.log(`socket 用戶離開 ${client.id}`);
+//   });
+// });
 
 http.listen(443, () => {
   console.log('listening on *:443');
---
+++ public/room.js
@@ -0,0 +1,352 @@
+$(function() {
+    var FADE_TIME = 150; // ms
+    var TYPING_TIMER_LENGTH = 400; // ms
+    var COLORS = [
+    '#e21400', '#91580f', '#f8a700', '#f78b00',
+    '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
+    '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
+    ];
+  
+    // Initialize variables
+    var $window = $(window);
+    var $usernameInput = $('.usernameInput'); // Input for username
+    var $messages = $('.messages'); // Messages area
+    var $inputMessage = $('.inputMessage'); // Input message input box
+  
+    var $loginPage = $('.login.page'); // The login page
+    var $chatPage = $('.chat.page'); // The chatroom page
+    var $joinGame = $('.joinGame'); 
+    var $leaveGame = $('.leaveGame'); 
+  
+    // Prompt for setting a username
+    var username;
+    var connected = false;
+    var typing = false;
+    var lastTypingTime;
+    var $currentInput = $usernameInput.focus();
+  
+    var socket = io();
+  
+    function addParticipantsMessage (data) {
+      var message = '';
+      if (data.numUsers === 1) {
+        message += "目前沒有人在線上，轉傳網址邀請好友上來玩！";
+      } else {
+        message += "這裡有 " + data.numUsers + "  位用戶在等待配對，按開始->交朋友！";
+      }
+      log(message);
+    }
+  
+    // Sets the client's username
+    function setUsername () {
+      username = cleanInput($usernameInput.val().trim());
+  
+      // If the username is valid
+      if (username) {
+        $loginPage.fadeOut();
+        $chatPage.show();
+        $loginPage.off('click');
+        $currentInput = $inputMessage.focus();
+  
+        // Tell the server your username
+        socket.emit('add user', username);
+        
+      }
+    }
+  
+    // Sends a chat message
+    function sendMessage () {
+      var message = $inputMessage.val();
+      // Prevent markup from being injected into the message
+      message = cleanInput(message);
+      // if there is a non-empty message and a socket connection
+      if (message && connected) {
+        $inputMessage.val('');
+        addChatMessage({
+          username: username,
+          message: message
+        });
+        // tell server to execute 'new message' and send along one parameter
+        socket.emit('new message', message);
+      }
+    }
+  
+    // Log a message
+    function log (message, options) {
+      var $el = $('<li>').addClass('log').text(message);
+      addMessageElement($el, options);
+    }
+  
+  
+  
+  
+  
+  
+  
+  
+    // Adds the visual chat message to the message list
+    function addChatMessage (data, options) {
+      // Don't fade the message in if there is an 'X was typing'
+      var $typingMessages = getTypingMessages(data);
+      options = options || {};
+      if ($typingMessages.length !== 0) {
+        options.fade = false;
+        $typingMessages.remove();
+      }
+  
+      var $usernameDiv = $('<span class="username"/>')
+      .text(data.username)
+      .css('color', getUsernameColor(data.username));
+      var $messageBodyDiv = $('<span class="messageBody">')
+      .text(data.message);
+  
+      var typingClass = data.typing ? 'typing' : '';
+      var $messageDiv = $('<li class="message"/>')
+      .data('username', data.username)
+      .addClass(typingClass)
+      .append($usernameDiv, $messageBodyDiv);
+  
+      addMessageElement($messageDiv, options);
+    }
+  
+    // Adds the visual chat typing message
+    function addChatTyping (data) {
+      data.typing = true;
+      data.message = 'is typing';
+      addChatMessage(data);
+    }
+  
+    // Removes the visual chat typing message
+    function removeChatTyping (data) {
+      getTypingMessages(data).fadeOut(function () {
+        $(this).remove();
+      });
+    }
+  
+    // Adds a message element to the messages and scrolls to the bottom
+    // el - The element to add as a message
+    // options.fade - If the element should fade-in (default = true)
+    // options.prepend - If the element should prepend
+    //   all other messages (default = false)
+    function addMessageElement (el, options) {
+      var $el = $(el);
+  
+      // Setup default options
+      if (!options) {
+        options = {};
+      }
+      if (typeof options.fade === 'undefined') {
+        options.fade = true;
+      }
+      if (typeof options.prepend === 'undefined') {
+        options.prepend = false;
+      }
+  
+      // Apply options
+      if (options.fade) {
+        $el.hide().fadeIn(FADE_TIME);
+      }
+      if (options.prepend) {
+        $messages.prepend($el);
+      } else {
+        $messages.append($el);
+      }
+      $messages[0].scrollTop = $messages[0].scrollHeight;
+    }
+  
+    // Prevents input from having injected markup
+    function cleanInput (input) {
+      return $('<div/>').text(input).text();
+    }
+  
+    // Updates the typing event
+    function updateTyping () {
+      if (connected) {
+        if (!typing) {
+          typing = true;
+          socket.emit('typing');
+        }
+        lastTypingTime = (new Date()).getTime();
+  
+        setTimeout(function () {
+          var typingTimer = (new Date()).getTime();
+          var timeDiff = typingTimer - lastTypingTime;
+          if (timeDiff >= TYPING_TIMER_LENGTH && typing) {
+            socket.emit('stop typing');
+            typing = false;
+          }
+        }, TYPING_TIMER_LENGTH);
+      }
+    }
+  
+    // Gets the 'X is typing' messages of a user
+    function getTypingMessages (data) {
+      return $('.typing.message').filter(function (i) {
+        return $(this).data('username') === data.username;
+      });
+    }
+  
+    // Gets the color of a username through our hash function
+    function getUsernameColor (username) {
+      // Compute hash code
+      var hash = 7;
+      for (var i = 0; i < username.length; i++) {
+       hash = username.charCodeAt(i) + (hash << 5) - hash;
+     }
+      // Calculate color
+      var index = Math.abs(hash % COLORS.length);
+      return COLORS[index];
+    }
+  
+    // Keyboard events
+  
+    $window.keydown(function (event) {
+      // Auto-focus the current input when a key is typed
+      if (!(event.ctrlKey || event.metaKey || event.altKey)) {
+        $currentInput.focus();
+      }
+      // When the client hits ENTER on their keyboard
+      if (event.which === 13) {
+        if (username) {
+          sendMessage();
+          socket.emit('stop typing');
+          typing = false;
+        } else {
+          setUsername();
+        }
+      }
+    });
+  
+    $inputMessage.on('input', function() {
+      updateTyping();
+    });
+  
+    // Click events
+  
+    // Focus input when clicking anywhere on login page
+    $loginPage.click(function () {
+      $currentInput.focus();
+    });
+  
+    // Focus input when clicking on the message input's border
+    $inputMessage.click(function () {
+      $inputMessage.focus();
+    });
+  
+  
+    $joinGame.click(function () {
+      joinGame();
+  
+    })
+  
+    $leaveGame.click(function () {
+      leaveGame();
+  
+    })
+  
+    // Socket events
+  
+    // Whenever the server emits 'login', log the login message
+    socket.on('login', function (data) {
+      connected = true;
+      // Display the welcome message
+      var message = "歡迎來到 'Chatting Booth' 尬聊小站 ";
+      log(message, {
+        prepend: true
+      });
+      addParticipantsMessage(data);
+    });
+  
+    // Whenever the server emits 'new message', update the chat body
+    socket.on('new message', function (data) {
+      addChatMessage(data);
+    });
+  
+  
+  
+    // Whenever the server emits 'user joined', log it in the chat body
+    socket.on('user joined', function (data) {
+      log(data.username + ' joined');
+      addParticipantsMessage(data);
+    });
+  
+    // Whenever the server emits 'user left', log it in the chat body
+    socket.on('user left', function (data) {
+      log(data.username + ' left');
+      addParticipantsMessage(data);
+      removeChatTyping(data);
+  
+  
+    });
+  
+    // Whenever the server emits 'typing', show the typing message
+    socket.on('typing', function (data) {
+      addChatTyping(data);
+    });
+  
+  
+    // Whenever the server emits 'stop typing', kill the typing message
+    socket.on('stop typing', function (data) {
+      removeChatTyping(data);
+    });
+  
+  
+  
+  
+    socket.on('gameCreated', function (data) {
+      console.log("Game Created! ID is: " + data.gameId)
+      log(data.username + ' 開始了一場尬聊: ' + data.gameId);
+      log(' 系統努力配對中，我們也討厭等待..');
+      //alert("Game Created! ID is: "+ JSON.stringify(data));
+    });
+    
+    socket.on('disconnect', function () {
+     log('抱歉..您已斷線，重開看看吧！');
+   });
+    
+    socket.on('reconnect', function () {
+     log('恭喜！！您成功回來了~');
+     if (username) {
+       socket.emit('add user', username);
+     }
+   });
+    
+    socket.on('reconnect_error', function () {
+     log('系統自爆程序啟動');
+   });
+  
+  
+  //Join into an Existing Game
+  function joinGame(){
+    socket.emit('joinGame');
+  };
+  
+  socket.on('joinSuccess', function (data) {
+    log('High Five！恭喜成功加入了一場尬聊: ' + data.gameId);
+  });
+  
+  
+  //Response from Server on existing User found in a game
+  socket.on('alreadyJoined', function (data) {
+    log('您已經在尬聊中了，對方太無趣嗎?那->換一個');
+  });
+  
+  
+  function leaveGame(){
+    socket.emit('leaveGame');
+  };
+  
+  socket.on('leftGame', function (data) {
+    log('太尷尬所以離開了尬聊 ' + data.gameId);
+  });
+  
+  socket.on('notInGame', function () {
+    log('您還沒開始新的尬聊->交朋友');
+  });
+  
+  socket.on('gameDestroyed', function (data) { 
+    log( data.gameOwner+ ' 結束了這場尬聊 ' + data.gameId);
+  
+  });
+  
+  });
