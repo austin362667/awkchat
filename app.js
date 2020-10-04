c0f6e6055b9802e36eade8eb881cb09f026acd64
austin362667 <austin362667@gmail.com>
2020-10-04 21:44:20 +0800

    fix video

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = require('express')();
 var fs = require('fs');
+
 // var enforce = require('express-sslify');
 // const redis = require("redis");
 // const path = require("path");
@@ -17,18 +18,21 @@ var loopLimit = 0;
 // room = 0;
 // match = [];
 
-var options = {
-  key: fs.readFileSync('/etc/letsencrypt/live/lattemall.company/privkey.pem'),
-  ca: [fs.readFileSync('/etc/letsencrypt/live/lattemall.company/fullchain.pem')],
-  cert: fs.readFileSync('/etc/letsencrypt/live/lattemall.company/fullchain.pem')
-};
-var https = require('https')
-const server = https.createServer(options,app);
+// var options = {
+//   key: fs.readFileSync('/etc/letsencrypt/live/lattemall.company/privkey.pem'),
+//   ca: [fs.readFileSync('/etc/letsencrypt/live/lattemall.company/fullchain.pem')],
+//   cert: fs.readFileSync('/etc/letsencrypt/live/lattemall.company/fullchain.pem')
+// };
+// var https = require('https')
+// const server = https.createServer(options,app);
 
-// var http = require('http')
-// const server = http.createServer(app);
+var http = require('http')
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+const server = http.createServer(app);
 
 var io = require('socket.io')(server);
+require('./router')(app, '15.165.187.225');
 
 const db = require('./queries')
 
@@ -39,12 +43,12 @@ app.put('/users/:id', db.updateUser)
 app.delete('/users/:id', db.deleteUser)
 
 
-app.use(function(req, res, next) {
-  if(!req.secure) {
-    return res.redirect(['https://', req.get('Host'), req.url].join(''));
-  }
-  next();
-});
+// app.use(function(req, res, next) {
+//   if(!req.secure) {
+//     return res.redirect(['https://', req.get('Host'), req.url].join(''));
+//   }
+//   next();
+// });
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname+'/public/index.html');
@@ -114,9 +118,9 @@ app.use(express.static('public'))
 //   }
 // });
 
-app.get('/video', (req, res) => {
-  res.sendFile(__dirname + '/public/video.html');
-});
+// app.get('/video', (req, res) => {
+//   res.sendFile(__dirname + '/public/video.html');
+// });
 
 io.on('connection', (socket) => {
   console.log('a user connected');
@@ -451,6 +455,59 @@ io.on('connection', client => {
   client.on('disconnect', () => {
     console.log(`socket 用戶離開 ${client.id}`);
   });
+});
+
+//vc2
+io.sockets.on('connection', function (socket){
+    
+	function log(){
+        var array = [">>> Message from server: "];
+        for (var i = 0; i < arguments.length; i++) {
+	  	    array.push(arguments[i]);
+        }
+	    socket.emit('log', array);
+	}
+
+	socket.on('message', function (message) {
+		log('Got message: ', message);
+		log('socket.room: ', socket.room);
+        socket.broadcast.to(socket.room).emit('message', message);
+	});
+    
+	socket.on('create or join', function (message) {
+		log('Got message create or join: ', message);
+        var room = message.room;
+        socket.room = room;
+        var participantID = message.from;
+        configNameSpaceChannel(participantID);
+        
+		var numClients = io.sockets.clients(room).length;
+
+		log('Room ' + room + ' has ' + numClients + ' client(s)');
+		log('Request to create or join room', room);
+
+		if (numClients == 0){
+			socket.join(room);
+			socket.emit('created', room);
+		} else {
+			io.sockets.in(room).emit('join', room);
+			socket.join(room);
+			socket.emit('joined', room);
+		}
+	});
+    
+    // Setup a communication channel (namespace) to communicate with a given participant (participantID)
+    function configNameSpaceChannel(participantID) {
+        var socketNamespace = io.of('/'+participantID);
+        
+        socketNamespace.on('connection', function (socket){
+            socket.on('message', function (message) {
+                // Send message to everyone BUT sender
+                socket.broadcast.emit('message', message);
+            });
+        });
+    }
+
 });
 
 //start and listen server
