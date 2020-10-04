e6e1665eaeb724d67c5da32582ede8a26c440988
austin362667 <austin362667@gmail.com>
2020-10-04 22:30:41 +0800

    fix video

--- app.js
+++ app.js
@@ -436,41 +436,41 @@ io.on('connection', function (socket) {
 
 
 //video chat
-function findNowRoom(client) {
-  return Object.keys(client.rooms).find(item => {
-    return item !== client.id
-  });
-}
+// function findNowRoom(client) {
+//   return Object.keys(client.rooms).find(item => {
+//     return item !== client.id
+//   });
+// }
 
-io.on('connection', client => {
-  console.log(`socket 用戶連接 ${client.id}`);
+// io.on('connection', client => {
+//   console.log(`socket 用戶連接 ${client.id}`);
 
-  client.on('joinRoom', room => {
-    console.log(room);
+//   client.on('joinRoom', room => {
+//     console.log(room);
     
-    const nowRoom = findNowRoom(client);
-    if (nowRoom) {
-      client.leave(nowRoom);
-    }
-    client.join(room, () => {
-      io.sockets.in(room).emit('roomBroadcast', '已有新人加入聊天室！');
-    });
-  });
+//     const nowRoom = findNowRoom(client);
+//     if (nowRoom) {
+//       client.leave(nowRoom);
+//     }
+//     client.join(room, () => {
+//       io.sockets.in(room).emit('roomBroadcast', '已有新人加入聊天室！');
+//     });
+//   });
 
-  client.on('peerconnectSignaling', message => {
-    console.log('接收資料：', message);
+//   client.on('peerconnectSignaling', message => {
+//     console.log('接收資料：', message);
  
-    const nowRoom = findNowRoom(client);
-    client.to(nowRoom).emit('peerconnectSignaling', message)
-  });
+//     const nowRoom = findNowRoom(client);
+//     client.to(nowRoom).emit('peerconnectSignaling', message)
+//   });
 
-  client.on('disconnect', () => {
-    console.log(`socket 用戶離開 ${client.id}`);
-  });
-});
+//   client.on('disconnect', () => {
+//     console.log(`socket 用戶離開 ${client.id}`);
+//   });
+// });
 
 //vc2
-io.sockets.on('connection', function (socket){
+io.on('connection', function (socket) {
     
 	function log(){
         var array = [">>> Message from server: "];
