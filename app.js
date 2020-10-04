2a58faab50f43ec14e11de6f7ef78b72dd000c96
austin362667 <austin362667@gmail.com>
2020-10-04 21:45:32 +0800

    fix video

--- app.js
+++ app.js
@@ -18,18 +18,19 @@ var loopLimit = 0;
 // room = 0;
 // match = [];
 
-// var options = {
-//   key: fs.readFileSync('/etc/letsencrypt/live/lattemall.company/privkey.pem'),
-//   ca: [fs.readFileSync('/etc/letsencrypt/live/lattemall.company/fullchain.pem')],
-//   cert: fs.readFileSync('/etc/letsencrypt/live/lattemall.company/fullchain.pem')
-// };
-// var https = require('https')
-// const server = https.createServer(options,app);
+var options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/lattemall.company/privkey.pem'),
+  ca: [fs.readFileSync('/etc/letsencrypt/live/lattemall.company/fullchain.pem')],
+  cert: fs.readFileSync('/etc/letsencrypt/live/lattemall.company/fullchain.pem')
+};
+var https = require('https')
+const server = https.createServer(options,app);
 
 var http = require('http')
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
 const server = http.createServer(app);
+
+// app.set('view engine', 'ejs');
+// app.engine('html', require('ejs').renderFile);
 
 var io = require('socket.io')(server);
 require('./router')(app, '15.165.187.225');
@@ -43,12 +44,12 @@ app.put('/users/:id', db.updateUser)
 app.delete('/users/:id', db.deleteUser)
 
 
-// app.use(function(req, res, next) {
-//   if(!req.secure) {
-//     return res.redirect(['https://', req.get('Host'), req.url].join(''));
-//   }
-//   next();
-// });
+app.use(function(req, res, next) {
+  if(!req.secure) {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  next();
+});
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname+'/public/index.html');
