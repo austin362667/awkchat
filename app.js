3e764440bea66d8290ad1eea2e953a098e3a78b5
austin362667 <austin362667@gmail.com>
2020-10-04 23:49:42 +0800

    fix video

--- app.js
+++ app.js
@@ -69,6 +69,7 @@ app.get('/video/:room', (req, res) => {
 
 io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
+    console.log(roomId, userId)
     socket.join(roomId)
     socket.to(roomId).broadcast.emit('user-connected', userId)
 
